9565428f1f6b981bfa63e242e465c6fd4fdf38ec
guisteel2 <40364455+guisteel2@users.noreply.github.com>
2020-09-20 16:33:38 -0300

    Delete md5.php

--- md5.php
+++
@@ -1,9 +0,0 @@
-<?php  //md5.php
-
-	echo "Fema@2020: " . md5("Fema@2020");
-	echo "<br><br>";
-	echo "Imesa@2020: " . md5("Imesa@2020");
-	echo "<br><br>";
-	echo "Almir Rogerio Camolesi: " . md5("Almir Rogerio Camolesi");
-
-?>
